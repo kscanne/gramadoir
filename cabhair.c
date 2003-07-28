f347f01c78e52d508182ce2e38f6e1ed08a48b22
Kevin Scannell <kscanne@gmail.com>
2003-07-28 21:07:33 +0000

    Changes which meld together "replacement" strings into a single comma-separated entry.   Executable "cabhair" is now used for developer creation of both focail.bs and eile.bs ("cabhair 0", "cabhair 1" respectively)

--- cabhair.c
+++ cabhair.c
@@ -15,29 +15,51 @@
 /* trouble */
 #endif
 
-int main()
+/* ngearr-chlóscríobhneoireachta  has 28 */
+#define GR_WORDMAX 32       
+
+int main(int argc, char* argv[])
    {
     const char unusedcode='\n';
-    char token[32], prev[32];   /* ngearr-chlóscríobhneoireachta  has 28 */
+    char token[GR_WORDMAX], prev[GR_WORDMAX], repl[GR_WORDMAX];
     int code,m;
     char codec;
-    scanf("%s", prev);
-    scanf("%d", &code);   codec = (char) code;
-    printf("%s%c%c",prev,unusedcode,codec); 
-    while (scanf("%s", token) != EOF) {
-          scanf("%d", &code);   codec = (char) code;
-	  if (!strcmp(prev,token)) {
-	         printf("%c",codec);
-	        }
-          else {
-	        m=0;
-	        while (prev[m]) {
-		     if (prev[m]==token[m]) m++;
-		     else break;
-		    }
-	        printf("%c%d%s%c%c",unusedcode,m,token+m,unusedcode,codec); 
-	        strcpy(prev,token);
-	       }
+
+    if (argc != 2) {
+        fprintf(stderr, "Úsáid: cabhair [0|1]\n");
+	return 1;
+       }
+    if (argv[1][0]=='0') {   /* focail.bs */
+  	  scanf("%s", prev);
+   	  scanf("%d", &code);   codec = (char) code;
+   	  printf("%s%c%c",prev,unusedcode,codec); 
+   	  while (scanf("%s", token) != EOF) {
+    	      scanf("%d", &code);   codec = (char) code;
+		  if (!strcmp(prev,token)) {
+		         printf("%c",codec);
+		        }
+     	          else {
+		        m=0;
+		        while (prev[m]) {
+			     if (prev[m]==token[m]) m++;
+			     else break;
+			    }
+		        printf("%c%d%s%c%c",unusedcode,m,token+m,unusedcode,codec); 
+		        strcpy(prev,token);
+		       }
+      	     }
+	 }
+    else {
+          strcpy(prev, "");
+   	  while (scanf("%s %s", token, repl) != EOF) {
+	       if (!strcmp(prev, token)) {
+	             printf(",_%s", repl);
+	            }
+               else {
+	             printf("\n%s %s", token, repl);
+		     strcpy(prev, token);
+	            }
+	      }
          }
     return 0;
    }
