fa1b9e7b78b39db7ec264c4329bc4a8708e28f5d
Kevin Scannell <kscanne@gmail.com>
2004-07-19 03:24:27 +0000

    Added install comments for Diarmaid.

--- gr.pl
+++ gr.pl
@@ -7,3 +7,22 @@ my @textinput = <>;
 my $toserver = "@textinput";
 from_to($toserver, "iso-8859-1", "utf-8");
 print $server->call('gaeilge.gramadoir', $toserver);
+
+
+
+
+# (1) Download and install Perl for Windows from
+# http://www.activestate.com/Products/ActivePerl/
+# 
+# (2) Open the "Command Prompt" (DOS)
+# (3) Run the perl package manager:
+#     C:\>ppm
+# (4) Install the client RPC package:
+#     ppm> install Frontier-RPC
+# (5) Get gr.pl from me
+# (6) Run it, enter some text, and type "Ctrl-Z", then Enter:
+#     C:\>perl gr.pl
+#     Ta tu in ann.
+#     ^Z
+# 
+#   and you should see the XML error messages served from borel.slu.edu
