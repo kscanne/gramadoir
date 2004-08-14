c4ffa5c421c2af37eb7df30ae3ac3a5db0d0a1d4
Kevin Scannell <kscanne@gmail.com>
2004-08-14 21:47:26 +0000

    Build scripts extracted from old buildperl.

---
+++ po2perl.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Locale::PO;
+
+sub massage
+{
+	my ( $string ) = @_;
+
+	$string =~ s/@/\\@/g;
+	$string =~ s/\\\\n/\\n/g;
+	$string =~ s/\[/~[/g;
+	$string =~ s/\]/~]/g;
+	$string =~ s/\%s/[_1]/;
+	$string =~ s#\\\\/([A-Za-z]+)\\\\/#/$1/#;
+	$string =~ s#\\\\/\\\\([1-9])\\\\/#/[_$1]/#;
+
+	return $string;
+}
+
+if (@ARGV != 2) {
+	die "Usage: $0 POFILE LANGUAGECODE\n";
+}
+my $aref = Locale::PO->load_file_asarray($ARGV[0]);
+shift(@$aref);
+my $lang = $ARGV[1];
+
+print 'package Lingua::@TEANGA@::Gramadoir::Languages::'."$lang;\n\n";
+print 'use strict;'."\n";
+print 'use warnings;'."\n";
+print 'use utf8;'."\n";
+print 'use base qw(Lingua::@TEANGA@::Gramadoir::Languages);'."\n";
+print 'use vars qw(%Lexicon);'."\n\n";
+
+print '%Lexicon = ('."\n";
+
+foreach my $msg (@$aref) {
+	my $id = $msg->msgid();
+	my $str = $msg->msgstr();
+	if (defined($id) && defined($str)) {
+	$id = massage($id);
+	$str = massage($str);
+	if ($str eq '""') {
+		$str = $id;
+	}
+	print "    ";
+	print $id;
+	print "\n";
+	print " => ";
+	print $str;
+	print ",\n\n";
+	}
+}
+
+print ");\n";
+print "1;\n";
+
+exit 0;
