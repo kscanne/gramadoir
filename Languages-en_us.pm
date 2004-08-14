a6071eaf3a68434444e1fb839e8d42fb6ef55bb8
Kevin Scannell <kscanne@gmail.com>
2004-08-14 21:46:44 +0000

    Locale::Maketext components.

---
+++ Languages-en_us.pm
@@ -0,0 +1,13 @@
+package Lingua::@TEANGA@::Gramadoir::Languages::en_us;
+
+use strict;
+use warnings;
+use base qw(Lingua::@TEANGA@::Gramadoir::Languages);
+use vars qw(%Lexicon);
+
+%Lexicon = (
+	'_AUTO' => 1,
+
+
+);
+1;
---
+++ Languages.pm
@@ -0,0 +1,8 @@
+package Lingua::@TEANGA@::Gramadoir::Languages;
+
+use strict;
+use warnings;
+use Locale::Maketext 1.01;
+use base qw(Locale::Maketext);
+
+1;
