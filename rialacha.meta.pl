400f90e587c9a338c77eb0c7f6c732d3ad122341
Kevin Scannell <kscanne@gmail.com>
2005-01-19 16:19:00 +0000

    "OK" syntax added.

--- rialacha.meta.pl
+++ rialacha.meta.pl
@@ -7,6 +7,19 @@
 # This is free software; see the file COPYING for copying conditions.  There is
 # NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-s/^(.+[^?]):(.*)/s\/(?<![<>])($1)(?![<>])\/<E msg="$2">\$1<\\\/E>\/g;/;
 s/\|<B><Z>[^+]+\+<\\\/Z>[^\/]+\/B>//g;
 s/\(\?:<\[\\\/A-DF-Z\]\[^>\]\*>\)\+/<[A-DF-Z][^>]*>/g;
+if (/:OK *$/) {
+#	s/^/s\/<E[^>]*>(/;
+#	s/:OK *$/)<\\\/E>\/\$1\/g;/;
+	unless (/<E>/) {
+		s/^/<E>/;
+		s/:OK/<\\\/E>:OK/;
+	}
+	s/<E>/<E[^>]*>/;
+	s/^/s\/(/;
+	s/:OK *$/)\/strip_errors(\$1);\/eg;/;
+}
+else {
+	s/^(.+[^?]):(.*)/s\/(?<![<>])($1)(?![<>])\/<E msg="$2">\$1<\\\/E>\/g;/;
+}
