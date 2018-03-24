7df54175d9bdde5004365a337e23910a189bb61b
Kevin Scannell <kscanne@gmail.com>
2018-03-24 05:13:05 +0000

    was killing TG4, etc. accidentally

--- ga/tolatin1.sh
+++ ga/tolatin1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # encoding of this file is UTF-8; that's important.
-LC_ALL=C grep -v "[^'a-zA-ZáéíóúÁÉÍÓÚâãäçèêëìîñòöü /-]" | iconv -f UTF-8 -t iso-8859-1
+LC_ALL=C grep -v "[^'0-9a-zA-ZáéíóúÁÉÍÓÚâãäçèêëìîñòöü /-]" | iconv -f UTF-8 -t iso-8859-1
